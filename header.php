3913013a2779fc4bf09089ec477a4defe0d66801
Julie Pagano <julie.pagano@gmail.com>
2012-05-02 22:50:38 -0400

    Included header stylesheet calls from main site

--- header.php
+++ header.php
@@ -5,8 +5,18 @@
     <title><?php wp_title(); ?> <?php bloginfo( 'name' ); ?></title>
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" media="screen" />
+
+    <!-- Imported from main site -->
+    <link rel="stylesheet" href="<?php bloginfo( 'template_directory' ); ?>/stylesheets/site.css" type="text/css" media="screen" />
+    <link href="<?php bloginfo( 'template_directory' ); ?>/stylesheets/fonts/bebas_neue/stylesheet.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="<?php bloginfo( 'template_directory' ); ?>/stylesheets/fonts/league_gothic/stylesheet.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="<?php bloginfo( 'template_directory' ); ?>/stylesheets/skeleton/base.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="<?php bloginfo( 'template_directory' ); ?>/stylesheets/skeleton/skeleton.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="<?php bloginfo( 'template_directory' ); ?>/stylesheets/skeleton/layout.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="<?php bloginfo( 'template_directory' ); ?>/stylesheets/site.css" media="screen" rel="stylesheet" type="text/css" />
+
+
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
     <?php wp_head(); ?>
   </head>
-  <body>
