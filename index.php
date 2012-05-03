2d1607408cb996af3de985a290a3c055a7c13a93
Julie Pagano <julie.pagano@gmail.com>
2012-05-02 23:03:39 -0400

    Include basic layout for main page content

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php get_header(); ?>
 
-TESTING
+    <div id='main' role='main'>
+      <div id='page_title'>
+        <div class='container'>
+          <h1>BLOG</h1>
+        </div>
+      </div>
+      <div class='container'>
+        TESTING
+      </div>
+    </div>
 
 <?php get_footer(); ?>
