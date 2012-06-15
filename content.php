1f0c27c6067dc8c2b1429ee53eb9b88455adbbd4
Julie Pagano <julie.pagano@gmail.com>
2012-06-14 22:50:57 -0400

    Incremental improvements for style and layout

--- content.php
+++ content.php
@@ -12,13 +12,26 @@
       </span>
     </div>
   </header>
-  <div class="entry-timestamp">
-    <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>
-  </div>
-  <div class="entry-categories">
-    <?php the_category(', '); ?>
-  </div>
   <?php the_content(); ?>
+  <footer>
+    <div class="entry-categories">
+      <ul>
+        <li>
+          <strong>Categories:</strong>
+        </li>
+        <?php foreach((get_the_category()) as $category) { ?>
+          <li>
+            <a href="<?= get_category_link($category->cat_ID) ?>">
+              <?= $category->cat_name ?>
+            </a>
+          </li>
+        <?php } ?>
+      </ul>
+    </div>
+    <div class="entry-timestamp">
+      <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>
+    </div>
+  </footer>
   <section class="comments">
     <?php comments_template(); ?>
   </section>
